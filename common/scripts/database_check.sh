c4055f13b5cdf438ba8c278d782c2a9d05ea5e37
Benny <scetbu.b@gmail.com>
2024-12-10 17:22:22 +0100

    fix database_check.sh

--- common/scripts/database_check.sh
+++ common/scripts/database_check.sh
@@ -5,4 +5,4 @@ set -e
 cd "$(dirname "$0")"
 . ./library.sh
 
-cd "$LIBRARY_SH_DIR../../backend" && bash "$LIBRARY_SH_DIR/run_against_psql.sh" ./cmd/database_check
+cd "$LIBRARY_SH_DIR/../../backend" && bash "$LIBRARY_SH_DIR/run_against_psql.sh" ./cmd/database_check
